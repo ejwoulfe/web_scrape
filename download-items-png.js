86d0f1a448a6dd9260265840505c5943d98632da
Ed Woulfe <47793125+ejwoulfe@users.noreply.github.com>
2019-04-18 00:07:34 -0500

    Cleaned code and renamed files
    Made the code a lot easier to read. 
    Renamed files so it's easier to know what each does.
    Made a loop to run the csv functionality instead of copy pasting the code 5 times.

---
+++ download-items-png.js
@@ -0,0 +1,66 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const request = require("request");
+var csv = require('csv');
+var csv = require('fast-csv');
+var names = [];
+var links = [];
+
+function putDataIntoArray(name, link){
+  names.push(name);
+  links.push(link);
+}
+
+function getName(count, namesArr){
+  return namesArr[count];
+}
+
+function getLink(count, linksArr){
+  return linksArr[count];
+}
+
+function getArraysLength(arr){
+  return arr.length;
+}
+
+
+function downloadImages(){csv.fromPath('itemList.csv')
+.on('data', function(data) {
+  
+  putDataIntoArray(data[1], data[2]);
+})
+.on('end', function() {
+  
+  
+  (async () => {
+    
+    const browser = await puppeteer.launch({ headless: false });
+    for(var i = 0; i<getArraysLength(links);i++){
+      const page = await browser.newPage();
+      var url = String(getLink(i, links));
+      var file = String(getName(i, names));
+      
+      var viewSource = await page.goto(url);
+      await page.waitFor(2000);
+      const imageUrl = await page.evaluate(() =>
+      document.querySelector("img").src
+    ); 
+    
+    fs.writeFile("images/" + file + ".png", await viewSource.buffer(), function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      
+      console.log("The file was saved!");
+      
+    });
+    await page.close();
+  }
+  await browser.close();
+  
+})();
+
+})
+}
+
+downloadImages();
---
+++ items-scrape-for-download.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const url = 'https://bdocodex.com/us/items/materials/';
+const fs = require('fs');
+const { parse } = require('json2csv');
+
+
+function writeToCsv(array){
+  const csv = parse(array);
+  fs.appendFileSync("./itemList.csv", csv);
+}
+
+(async() =>{
+  const browser = await puppeteer.launch({headless:false});
+  
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 1200 })
+  
+  await page.goto(url, {waitUntil: 'domcontentloaded'});
+  await page.waitFor(1000);
+  await page.select('select.form-control', '200')
+  await page.waitFor(5000);
+  
+  for(var i = 0; i<5;i++){
+    var result = await page.evaluate(() =>
+    
+    
+    Array.from(document.querySelectorAll('tbody tr')).map(tr =>({
+      itemID: 1,
+      itemName: tr.querySelector('tbody tr b').textContent,
+      itemImage: tr.querySelector('tbody tr td.dt-icon img').src
+      
+    }))
+    
+  );
+  
+  writeToCsv(result);
+  await page.click('li.paginate_button.next');
+  await page.waitFor(2000);
+}
+
+await browser.close();
+
+
+})();
