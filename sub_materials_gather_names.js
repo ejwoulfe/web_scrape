a83a170f2040627496639cdf4246209d150ac891
Ed <edwoulfe@gmail.com>
2019-04-30 06:51:44 -0500

    File to go to the sub_material links and grab the names

--- sub_materials_gather_names.js
+++ sub_materials_gather_names.js
@@ -5,40 +5,35 @@ let writeStream = fs.createWriteStream('gathering_names.txt');
 
 
 
-
 fs.readFile('materials.txt', function(err, data) {
    if(err) throw err;
    var array = data.toString().split("\n");
    // console.log(array[0].replace(/"/g, '').split(",")[0])
-   
-   for(var i = 0; i<2;i++){
-     for(var z = 0; z< array[i].split(",").length;z++){
-       if(array[i].replace(/"/g, '').split(",")[z]==='\r'){
-         z+1;
-       }
-       else{
-        console.log(array[i].split(",")[z]) 
-      }
-  }
-   }
 
 async function run() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  
-  for(var i = 0; i<2;i++){
-    for(var z = 0; z< array[i].split(",").length;z++){ 
+
+  for(var i = 0; i<array.length;i++){
+    for(var z = 0; z< array[i].split(",").length;z++){
       if(array[i].replace(/"/g, '').split(",")[z]==='\r'){
         z+1;
       }
       else{
-       await page.goto(array[i].replace(/"/g, '').split(",")[z]);
+        const page = await browser.newPage();
+       await page.goto(array[i].replace(/"/g, '').split(",")[z], {waitUntil: 'domcontentloaded'});
+       const text = page.evaluate(() => document.querySelector('table.smallertext span.item_title b').innerHTML);
+       await page.waitFor(3000);
+       text.then(function(result){
+         writeStream.write(result.trim() + ", ");
+       })
+       await page.waitFor(3000);
+        await page.close();
      }
  }
- await page.waitFor(2000);
- console.log("end of first item")
-  }
-  
+ writeStream.write("\n");
+}
+
 
 
   browser.close();
