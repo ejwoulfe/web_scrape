431f45b17c4e6217644c80d1f7a577b2eb23424b
Ed <edwoulfe@gmail.com>
2019-05-12 20:12:35 -0500

    Update find_items_id.js

--- find_items_id.js
+++ find_items_id.js
@@ -17,7 +17,7 @@ con.connect(function(err) {
 var data = fs.readFileSync('Processing/Processing_sub_materials.txt', 'utf8');
 var hold = data.split("\r");
 //for(var i = 0; i < hold.length;i++){
-for(var i = 400; i < 500;i++){
+for(var i = 500; i < 600;i++){
     var itemList = (hold[i].split(","));
     for(var z = 0; z < itemList.length-1;z++){
       //console.log(itemList[z].trim());
