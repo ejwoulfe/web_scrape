08777a0713b3b9564f2156146a3404c64855aabe
Ed <edwoulfe@gmail.com>
2019-05-13 22:28:37 -0500

    Adding more items to the database
    Going through the processing recipes and still adding  the remaining sub materials. Close to the end.

--- replace_duplicates.js
+++ replace_duplicates.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { parse } = require('json2csv');
 var readline = require('readline');
 let writeStream = fs.createWriteStream('fast_sql.txt');
-var id = 2064;
+var id = 2105;
 
 
 fs.readFile('Processing/Processing_sub_materials.txt', function(err, data) {
@@ -12,8 +12,9 @@ fs.readFile('Processing/Processing_sub_materials.txt', function(err, data) {
   // for(var i = 0; i< array.length;i++) {
 //     let unique = [...new Set(array)];
      
-     for(var i = 517; i< 551;i++) {
+     for(var i = 571; i< 668;i++) {
        writeStream.write("(" + id++ + ', ' + '"' +  array[i].replace(",", '') + '",' + '"' + "../assets/item_images/" + array[i].replace(",", '') + '.png", ' + '"Base"'  + "),");
+       writeStream.write("\n");
  
      }
 
