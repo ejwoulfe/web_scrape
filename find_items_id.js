e3423076c82f4e524cbcbd38e03443cc44e8f097
Ed <edwoulfe@gmail.com>
2019-05-14 20:39:34 -0500

    Finished item database
    Found all item ids in the item database. Now need to format the items for the sub materials table.

--- find_items_id.js
+++ find_items_id.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var mysql = require('mysql');
-
+let writeStream = fs.createWriteStream('all_items_ids.txt');
+var arr = [];
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -16,21 +17,45 @@ con.connect(function(err) {
 
 var data = fs.readFileSync('Processing/Processing_sub_materials.txt', 'utf8');
 var hold = data.split("\r");
+
 //for(var i = 0; i < hold.length;i++){
-for(var i = 800; i < 900;i++){
-    var itemList = (hold[i].split(","));
-    for(var z = 0; z < itemList.length-1;z++){
-      //console.log(itemList[z].trim());
-      var item = itemList[z].trim();
-      con.query("SELECT material_id, material_name FROM materials_table WHERE material_name = " + "\"" + item + "\"", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
+// for(var i = 0; i < hold.length;i++){
+//     var itemList = (hold[i].split(","));
+//     for(var z = 0; z < itemList.length-1;z++){
+//       var item = itemList[0].trim();
+//       con.query("SELECT material_id FROM materials_table WHERE material_name = " + "\"" + item + "\"", function (err, result, fields) {
+//     if (err) throw err;
+//      writeStream.write(JSON.stringify(result));
+//      console.log(result);
+//
+//
+//
+//
+//   });
+//
+//  }
+// }
+// for (var i = 0; i < hold.length-1; i++) {
+//
+// for (var i = 0; i < 6; i++) {
+// var amountOfMaterials = (hold[i].split(",").length-1);
+//
+//  var split = (hold[i].split("\r"))
+//  for (var z = 0; z < split[0].split(",").length-1; z++) {
 
 
+   //
+   // arr.push(split[0].split(",")[z].trim());
+ con.query("SELECT * FROM materials_table", function (err, result, fields) {
+   // if any error while executing above query, throw error
+     if (err) throw err;
+     // if there is no error, you have the result
+     // iterate for all the rows in result
+     Object.keys(result).forEach(function(key) {
+       var row = result[key];
+       console.log(row.material_name)
+     });
+   });
 
-    }
-}
-
-
+con.end()
 });
